7f681ffbf5633b83dd188d18bee12168a923dff7
qjeanl <jean.louw@quirk.biz>
2015-12-15 15:56:32 +0200

    Cookie fix

--- grails-app/conf/steamapi/CookieFilterFilters.groovy
+++ grails-app/conf/steamapi/CookieFilterFilters.groovy
@@ -7,6 +7,9 @@ class CookieFilterFilters {
             before = {
                 //Checks for a cookie with the user's steamId and sets it in session if found
                 Utilities.checkCookieValue(request, session, grailsApplication.config.preference.cookieName, "steamId")
+                if(session.steamId == "null"){
+                    session.steamId = null
+                }
             }
         }
     }
